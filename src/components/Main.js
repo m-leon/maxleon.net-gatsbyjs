dc6c0edcebf5aaec823a6eb768f45f3f4af44d35
Max <max_leon@gmx.com>
2018-08-20 11:34:57 -0700

    - Vertically centered content on Main component - Added linebreaks at 'breakpoints' in Main to prevent jarring repositioning on smaller width screens

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,16 +2,18 @@ import React from 'react';
 import TextLoop from 'react-text-loop';
 
 const Main = () => (
-  <div className='content'>
-    <p id='main__tagline'>
-      I’m <span id='main__name'>Max Leon</span>, <TextLoop id='main__iam' speed={2000}>
+  <div className='content content__main'>
+    <span id='main__tagline'>
+      <p>I’m <span id='main__name'>Max Leon</span>,</p>
+      <TextLoop speed={2000}>
         <span>a full-stack web developer</span>
         <span>an app developer</span>
         <span>a python hacker</span>
         <span>a software engineer</span>
         <span>a bicyclist</span>
-      </TextLoop> from California.
-    </p>
+      </TextLoop>
+      <p>from California.</p>
+    </span>
     <span id='main__links'>
       <a href='https://github.com/m-leon'>GitHub</a> &bull; <a href='mailto:max@maxleon.net'>email</a>
     </span>
