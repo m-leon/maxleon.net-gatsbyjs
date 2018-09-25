77bdc7d2af2747d0e9cda612909657368ef37830
Max <max@maxleon.net>
2018-09-24 21:29:51 -0700

    - Removed unused tests

--- src/tests/components/NotFound.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import NotFound from '../../components/NotFound';
-
-it('render NotFound without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<NotFound />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/tests/components/Skills.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Skills from '../../components/Skills';
-
-it('render Skills without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Skills />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
