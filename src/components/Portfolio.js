2b0fc04cb5957651f87e9b32713f484f32730ab0
Max <max@maxleon.net>
2018-09-26 10:59:52 -0700

    - Added content & styling to the Portfolio component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
 const Portfolio = () => (
-  <section>
-    <h2>My Work</h2>
+  <section className="portfolio">
+    <h2 className="portfolio__title">My Work</h2>
+    <div className="portfolio__matrix">
+      <div className="portfolio__card portfolio__card--hush">
+        <div className="portfolio__overlay"><span>Hush</span></div>
+      </div>
+      <div className="portfolio__card portfolio__card--yoovo">
+        <div className="portfolio__overlay"><span>Yoovo</span></div>
+      </div>
+      <div className="portfolio__card portfolio__card--smart-start">
+        <div className="portfolio__overlay"><span>Smart Start Preschool</span></div>
+      </div>
+      <div className="portfolio__card portfolio__card--craigslist">
+        <div className="portfolio__overlay"><span>Craigslist Conceptual Redesign</span></div>
+      </div>
+    </div>
   </section>
 );
 
