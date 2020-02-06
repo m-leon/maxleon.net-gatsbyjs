cae7fdfd61afcad7b63d351803fb30db392429bc
Max <max@maxleon.net>
2020-02-06 15:21:27 -0800

    -Removed anlytics

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import configureAnalytics from '../configureAnalytics';
 import Landing from './Landing';
-import { AnalyticsProvider } from './AnalyticsContext';
-
-const analytics = configureAnalytics();
 
 const AppRouter = () => (
   <BrowserRouter>
-    <AnalyticsProvider analytics={analytics}>
-      <Switch>
-        <Route path="/" component={Landing} />
-      </Switch>
-    </AnalyticsProvider>
+    <Switch>
+      <Route path="/" component={Landing} />
+    </Switch>
   </BrowserRouter>
 );
 
